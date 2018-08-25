b713cfce2e0a8061ce98660a5be7de6a6350d1d2
gasalazaror <gasalazaror@gmail.com>
2018-08-25 13:00:15 -0500

    nuevo

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -9,18 +9,12 @@ const routes: Routes = [
         children: [
             { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
             { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
-            { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
-            { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
             { path: 'persona', loadChildren: './persona/persona.module#PersonaModule' },
             { path: 'vehiculo', loadChildren: './vehiculo/vehiculo.module#VehiculoModule' },
             { path: 'servicio', loadChildren: './servicio/servicio.module#ServicioModule' },
             { path: 'reporte', loadChildren: './reporte/reporte.module#ReporteModule' },
             { path: 'orden', loadChildren: './orden-trabajo/orden-trabajo.module#OrdenTrabajoModule' },
-            { path: 'forms', loadChildren: './form/form.module#FormModule' },
-            { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
-            { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
-            { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
-            { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
+         
         ]
     }
 ];
